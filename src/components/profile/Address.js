c096c52fedf1c35232583cf66e7db289677a89e6
Ryan <ryan.waits@gmail.com>
2022-08-24 21:15:39 -0500

    use truncateAddress to shorten address

--- src/components/profile/Address.js
+++ src/components/profile/Address.js
@@ -1,16 +1,14 @@
 import { useAtom } from 'jotai';
 import { useMemo } from 'react';
 import { stxAddressAtom, stxBnsNameAtom } from '../../store/stacks';
+import { truncateAddress } from '@stacks-os/utils';
 
 export function Address() {
   const [stxAddress] = useAtom(stxAddressAtom);
   const [bnsName] = useAtom(stxBnsNameAtom);
   const displayAddress = useMemo(() => {
     if (bnsName.loaded) return bnsName.data;
-    if (stxAddress.loaded)
-      return `${stxAddress.data.substring(0, 5)}...${stxAddress.data.substring(
-        stxAddress.data.length - 5
-      )}`;
+    if (stxAddress.loaded) return `${truncateAddress(stxAddress.data)}`;
     return 'Profile';
   }, [stxAddress, bnsName]);
 
